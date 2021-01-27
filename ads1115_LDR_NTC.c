32ca01af468e2cc2f3779ebe63f18600dc700ef1
Lukas Stix <44016590+StixLukas@users.noreply.github.com>
2021-01-27 09:08:20 +0100

    Add files via upload

---
+++ ads1115_LDR_NTC.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+	FILE* dataFromLDR = popen("gpio -x ads1115:120:0x49 aread 121","r");
+	int iLDR = 0;
+	fscanf(dataFromLDR, "%d",  &iLDR);
+	printf("LDR Output: %d\n",iLDR);
+	
+	FILE* dataFromNTC = popen("gpio -x ads1115:120:0x49 aread 122","r");
+	int iNTC = 0;
+	fscanf(dataFromNTC, "%d", &iNTC);
+	float fNTCTemperatur = (float)iNTC/1000;
+	printf("Temperatur: %.2f\n",fNTCTemperatur);
+
+	return 0;
+}

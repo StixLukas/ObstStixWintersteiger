ca3c99a49895ec7d7f3cc73dd0764dea01f0fc3b
Lukas Stix <44016590+StixLukas@users.noreply.github.com>
2021-01-27 09:08:27 +0100

    Add files via upload

---
+++ luxmeter.c
@@ -0,0 +1,11 @@
+#include <wiringPiI2C.h>
+#include <stdio.h>
+int main(void){
+	int handle = wiringPiI2CSetup(0x5C);
+	wiringPiI2CWrite(handle,0x01);
+	wiringPiI2CWrite(handle,0x21);
+	int word = wiringPiI2CReadReg16(handle,0x00);
+	int lux=((word & 0xff00)>>8) | ((word & 0x00ff)<<8);
+	printf("Aktuelle Beleuchtungsstärke in Lux: %d\n",lux);
+	return 0;
+}

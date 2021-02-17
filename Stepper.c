e92abe634d5b59f59c0a8ecb0b9f6bc7e895287c
Kim D. Obst <44016607+obstkim@users.noreply.github.com>
2021-02-17 11:56:30 +0100

    Add files via upload

--- Stepper.c
+++ Stepper.c
@@ -7,57 +7,45 @@
 
 int D=1000;
 
-unsigned m[][4]={
-  {14,15,17,18},
-  {27,22,23,24}
-};
+unsigned m[]={18,16,32,33};
 
 int b[8]={0b0001, 0b011, 0b0010, 0b0110, 0b0100, 0b1100, 0b1000, 0b1001};
 
-int M=sizeof(m)/sizeof(m[0]);
-int N=sizeof(m[0])/sizeof(m[0][0]);
-
 void gpiosWrite(unsigned *p, unsigned v)
 {
-  for(int i=0; i<N; ++i)
+  for(int i=0; i<4; ++i)
     digitalWrite( p[i], (v&(1<<i)) ? HIGH : LOW );
 }
 
-void hstep(int mn, unsigned v)
+void hstep(unsigned v)
 {
-  gpiosWrite(m[mn], b[v & 0x7]);
+  gpiosWrite(m, b[v & 0x7]);
 }
 
-void hstep2(int x, int y)
+void hstep2(int x)
 {
-  hstep(0,x); hstep(1,y); usleep(D);
+  hstep(x) usleep(D);
 }
 
 int main(int argc, char *argv[])
 {
-  int i,j,x=0,y=0,dx,dy;
-
-  assert(((argc+1)/2==2) || !"Format: sudo ./move dx dy [D]");
+  int i,x=0,dx;
  
   wiringPiSetupGpio();
   dx = atoi(argv[1]);
-  dy = atoi(argv[2]);
-  if (argc==4) {
-    D = atoi(argv[3]);
+  if (argc==3) {
+    D = atoi(argv[2]);
     D = (D<1000) ? 1000 : D;
   }
 
-  for(i=0; i<M; ++i)
-    for(j=0; j<N; ++j)
+  for(i=0; i<4; ++i)
     {
-      pinMode(m[i][j], PI_OUTPUT);
-      digitalWrite(m[i][j], LOW);
+      pinMode(m[i], PI_OUTPUT);
+      digitalWrite(m[i], LOW);
     }
 
-  hstep2(x,y);
+  hstep2(x);
 
-  for(i=0; i<dx; ++i) hstep2(++x,y);
-  for(i=0; i>dx; --i) hstep2(--x,y);
-  for(i=0; i<dy; ++i) hstep2(x,++y);
-  for(i=0; i>dy; --i) hstep2(x,--y);
+  for(i=0; i<dx; ++i) hstep2(++x);
+  for(i=0; i>dx; --i) hstep2(--x);
 }

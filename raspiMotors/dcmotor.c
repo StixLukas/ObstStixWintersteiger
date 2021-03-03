0465ca8463189822134259177b70f68057d12782
Jonas Wintersteiger <jonas622002@gmail.com>
2021-03-03 10:24:49 +0100

    puts

--- raspiMotors/dcmotor.c
+++ raspiMotors/dcmotor.c
@@ -11,15 +11,15 @@ void dcmotor(int powerPin, int fwdPin, int bwdPin, int power, int isforward);
 int main (int argc, char **argv)
 { 
 	if(argc==6){
-		printf("%s", *argv[1]);
+		puts(argv[1]);
 		int powerpin= (int) argv[1];
-		printf("%s", *argv[2]);
+		puts(argv[2]);
 		int forward= (int) argv[2];
-		printf("%s", *argv[3]);
+		puts(argv[3]);
 		int backward= (int) argv[3];
-		printf("%s", *argv[4]);
+		puts(argv[4]);
 		int power= (int) argv[4];
-		printf("%s", *argv[5]);
+		puts(argv[5]);
 		int isforward= (int) argv[5];
 
 		dcmotor(powerpin, forward, backward, power, isforward); // dcmotor(38, 40, 36, 300, 1);

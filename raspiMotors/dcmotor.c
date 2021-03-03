9bbfefa5d6a36104bd9455793177f57340107124
Jonas Wintersteiger <jonas622002@gmail.com>
2021-03-03 10:16:30 +0100

    dcmototr

--- raspiMotors/dcmotor.c
+++ raspiMotors/dcmotor.c
@@ -10,6 +10,7 @@ void dcmotor(int powerPin, int fwdPin, int bwdPin, int power, int isforward);
 
 int main (int argc, char **argv)
 { 
+	if(argc==5){}
 	int powerpin= (int) argv[1];
 	int forward= (int) argv[2];
 	int backward= (int) argv[3];
@@ -17,6 +18,10 @@ int main (int argc, char **argv)
 	int isforward= (int) argv[5];
 
 	dcmotor(powerpin, forward, backward, power, isforward); // dcmotor(38, 40, 36, 300, 1);
+	}
+	else{
+	printf("Please enter 5 arguments");
+	}
 }
 
 

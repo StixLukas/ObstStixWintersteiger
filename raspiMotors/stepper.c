d9e2ef7aa7afcc5e06ec7180e534f25d48abd86a
Jonas Wintersteiger <jonas622002@gmail.com>
2021-03-03 09:57:27 +0100

    stepper.c return

--- raspiMotors/stepper.c
+++ raspiMotors/stepper.c
@@ -1,9 +1,6 @@
 /*#include <stdio.h>
-
 #include <pigpio.h>
-
 #define STEP_DELAY 2000
-
 /*
 2000  7 RPM
 1500  9 RPM
@@ -118,83 +115,75 @@ int main(int argc, char * argv[])
 /*
 #include <stdio.h>
 #include <wiringPi.h>
-
-#define FORWARD		1
-#define BACKWARD	0
-
+#define FORWARD   1
+#define BACKWARD  0
 const int SPEED = 500;
 const int PINS[4] = {22,23,24,25};
 const bool STEPS[8][4] = {
-	{HIGH, LOW, HIGH, LOW},	//0
-	{HIGH, LOW, LOW, LOW},	//1
-	{HIGH, LOW, LOW, HIGH},	//2
-	{LOW, LOW, LOW, HIGH},	//3
-	{LOW, HIGH, LOW, HIGH},	//4
-	{LOW, HIGH, LOW, LOW},	//5
-	{LOW, HIGH, HIGH, LOW},	//6
-	{LOW, LOW, HIGH, LOW},	//7
-	};
-
+  {HIGH, LOW, HIGH, LOW}, //0
+  {HIGH, LOW, LOW, LOW},  //1
+  {HIGH, LOW, LOW, HIGH}, //2
+  {LOW, LOW, LOW, HIGH},  //3
+  {LOW, HIGH, LOW, HIGH}, //4
+  {LOW, HIGH, LOW, LOW},  //5
+  {LOW, HIGH, HIGH, LOW}, //6
+  {LOW, LOW, HIGH, LOW},  //7
+  };
 void initPins(const int index);
 void halfStep(int &index, bool dir);
 void debugPrint(void);
-
 int main(void)
 {
-	printf("CodeNova, Stepper Test\n");
-	int index = 0;		//step index counter
-	
-	wiringPiSetup(); 	//setup wireing pi
-	initPins(index);	//init pins, start at 0
-
-	//forward 10 halfsteps
-	printf("* Forward Test *\n");
-	for(int i = 0; i < 10; i++)
-	{	
-		halfStep(index, FORWARD);
-		delay(SPEED);
-		printf("index: %d\n", index);	//debug index counter
-	}
-	
-	//backward 10 halfsteps
-	printf("* Backward Test *\n");
-	for(int i = 0; i < 10; i++)
-	{
-		halfStep(index, BACKWARD);
-		delay(SPEED);
-		printf("index: %d\n", index);	//debug index counter
-	}	
-	
-	printf("Test FIN\n");
-	return 0;
+  printf("CodeNova, Stepper Test\n");
+  int index = 0;    //step index counter
+  
+  wiringPiSetup();  //setup wireing pi
+  initPins(index);  //init pins, start at 0
+  //forward 10 halfsteps
+  printf("* Forward Test *\n");
+  for(int i = 0; i < 10; i++)
+  { 
+    halfStep(index, FORWARD);
+    delay(SPEED);
+    printf("index: %d\n", index); //debug index counter
+  }
+  
+  //backward 10 halfsteps
+  printf("* Backward Test *\n");
+  for(int i = 0; i < 10; i++)
+  {
+    halfStep(index, BACKWARD);
+    delay(SPEED);
+    printf("index: %d\n", index); //debug index counter
+  } 
+  
+  printf("Test FIN\n");
+  return 0;
 }
-
 void initPins(const int index)
 {
-	for(int i = 0; i < 4; i++)
-	{
-		printf("init-ing pin %d, OUTPUT\n", PINS[i]);	//debug output
-		pinMode(PINS[i], OUTPUT);						//set IO to output
-		digitalWrite(PINS[i], STEPS[index][i]);			//write to IO
-	}
-	debugPrint();										//print IO state, debug
-	printf("index: %d\n", index);						//print index number, debug
+  for(int i = 0; i < 4; i++)
+  {
+    printf("init-ing pin %d, OUTPUT\n", PINS[i]); //debug output
+    pinMode(PINS[i], OUTPUT);           //set IO to output
+    digitalWrite(PINS[i], STEPS[index][i]);     //write to IO
+  }
+  debugPrint();                   //print IO state, debug
+  printf("index: %d\n", index);           //print index number, debug
 }
-
 void halfStep(int &index, bool dir)
 {
-	index = dir ? ((index+1)%8) : ((index+7)%8);	//add|sub index based on direction
-	
-	for(int i = 0; i < 4; i++)
-		digitalWrite(PINS[i], STEPS[index][i]);		//write to IO	
-	
-	debugPrint();									//print IO state, debug
+  index = dir ? ((index+1)%8) : ((index+7)%8);  //add|sub index based on direction
+  
+  for(int i = 0; i < 4; i++)
+    digitalWrite(PINS[i], STEPS[index][i]);   //write to IO 
+  
+  debugPrint();                 //print IO state, debug
 }
-
 void debugPrint(void)
 {
-	for(int i = 0; i < 4; i++)
-		printf("%d", digitalRead(PINS[i])); //read the new io state
-	printf("\n");
+  for(int i = 0; i < 4; i++)
+    printf("%d", digitalRead(PINS[i])); //read the new io state
+  printf("\n");
 }
 */
